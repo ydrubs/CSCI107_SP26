16c49e4316ee379ea4464089c9dfdcc6db0ed2f4
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-01-25 15:54:18 -0600

    initial commit

---
+++ Unit_0/0.0 algorithms.py
@@ -0,0 +1,21 @@
+# n = 676773687365874356
+# sum = 0
+#
+# for digit in str(n):
+#     # print(digit)
+#     sum += int(digit)
+#
+# print(sum)
+
+# ---------------------------
+n = 6776576453443432
+prime = False
+
+for i in range(2, int(n**0.5)):
+    if n % i == 0:
+        break
+    else:
+        prime = True
+
+print(f'Prime Status of {n}: {prime}')
+
