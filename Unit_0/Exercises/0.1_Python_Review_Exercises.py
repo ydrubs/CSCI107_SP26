f160467b1f8b939ebfe6a52f08c9e04ededcae72
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-01-26 08:19:45 -0600

    initial commit

---
+++ Unit_0/Exercises/0.1_Python_Review_Exercises.py
@@ -0,0 +1,41 @@
+"""
+Exercises:
+"""
+# 1.Create a variable with your age, increase it by 5, and print the result.
+
+
+
+
+
+# 2. Create a variable with your age. Check if it is even. Check if it is greater then 23.
+
+
+
+
+
+# 3. Create a list of three numbers and print each value using a for loop. One loop uses the index of the elements the other loop uses the elements themselves.
+
+
+
+
+
+# 4. Ask for a temperature and print whether it’s cold, warm, or hot based on its value.
+
+
+
+
+
+
+#5. Use a while loop to count down from 5 to 1 and print 'Liftoff!' at the end.
+
+
+
+
+
+#6. Write a function that takes two numbers as floats, adds them together, and returns the result. Then call the function.
+
+
+
+
+#7. Go to https://edabit.com/ sign up for an account and try one of the Python challenges.
+#   You choose the difficulty level and challenge you want to try. Explain (or copy/paste) the challenge here along with your solution code.
