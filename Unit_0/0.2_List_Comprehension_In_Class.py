11b285c78f6b27760575d9f8b87c1cb67e6665de
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-01-30 11:12:53 -0600

    initial commit

---
+++ Unit_0/0.2_List_Comprehension_In_Class.py
@@ -0,0 +1,85 @@
+"""0.1b - List Comprehension
+
+List comprehension in Python is a concise way to create lists from existing ones.
+It's a powerful tool to write cleaner, more readable code
+
+"""
+
+###########################################################
+"""Create a list of values from 0 to 99 using a for loop"""
+pass
+
+
+"""Use list comprehension to achieve the same thing"""
+pass
+
+
+###########################################################
+"""Filtering - Create a list of only even numbers between 0 and 99 using a for loop"""
+pass
+
+
+
+"""Filtering - Create a list of only even numbers between 0 and 99 using list comprehension"""
+pass
+
+
+
+###########################################################
+"""Mapping - Create a list that doubles every number between 0 and 99 using a for loop"""
+pass
+
+
+
+"""Mapping - Create a list that doubles every number between 0 and 99 using list comprehension"""
+pass
+
+
+
+
+###########################################################
+"""Combining mapping and filtering with a loop.
+Triple all the even numbers between 1 and 100"""
+pass
+
+
+
+"""Combining mapping and filtering with list comprehension
+Triple all the even numbers between 1 and 100"""
+pass
+
+
+###########################################################
+
+"""Using list comprehension to filter common elements of two lists
+First we'll make two lists - multiples of 3 and 5 up tp 100 using list comprehension"""
+pass
+
+
+"""Now we use a for loop to filter the common elements in both lists and write to a new list"""
+pass
+
+
+
+"""...using list comprehension to do the same"""
+pass
+
+"""
+    Try it:
+    First Letter Extractor:
+        Write a list comprehension that takes a list of words and returns a list containing only the first letter of each word.
+
+        Example: words = ["apple", "banana", "cherry", "ambarella"] 
+                Returns: ['a', 'b', 'c']
+
+    Extension: Only print the letter if the last letter and first letter are the same
+                Returns: [""ambarella"]
+
+"""
+
+###########################################################
+"""
+Use list comprehension to create a list of 5 tuples where the first element n each tuple is the integers 1 to 5 
+and the second element is it square number - for example:
+    [(1,1), (2,4), (3,9)....]
+"""
