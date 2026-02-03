9fca48d531deebe176c835d057a4e8bbaff64d49
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-03 12:21:29 -0600

    initial commit

---
+++ Unit_0/Exercises/0.3_Dictionary_Exercises.py
@@ -0,0 +1,27 @@
+"""
+0.3 Dictionary Practice
+
+    1) Given the following dictionary of favorite colors by person:
+
+            favorites = {
+            "Alice": "blue",
+            "Marcus": "green",
+            "Jenna": "purple"
+        }
+
+        Write code that prints Jenna’s favorite color using syntax that references a dictionary.
+
+    2) Using the same dictionary from Exercise 1, add a new person:
+
+        "Riley": "orange"
+
+        Then print the updated dictionary.
+
+    3) Marcus decides their new favorite color is yellow.
+        Update the dictionary so it reflects that change.
+
+
+    4) Create a new dictionary where the keys become values and the values become keys.
+
+         HINT - Use a for loop that loops through both the keys and values of the favorites dictionary
+"""
