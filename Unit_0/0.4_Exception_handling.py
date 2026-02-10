efd77e07a40cf72e91c6820d51418858d1672234
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-10 16:26:27 -0600

    initial commit

---
+++ Unit_0/0.4_Exception_handling.py
@@ -0,0 +1,142 @@
+"""
+Lesson 0.4 - Exception Handling
+
+Powerpoint Lesson: 0.4 - Exception Handling
+"""
+
+## Slide 1 ##
+"""
+If we don't handle exceptions, we can't ensure our program will NOT break
+    Here, we expect the user to enter an integer number. If they enter a different data type, the program will stop running. 
+"""
+
+
+
+## Slide 4 ##
+"""
+Syntax errors need to be debugged by the programmer because they cannot be caught at run-time
+"""
+
+
+
+"""
+Run-time errors CAN be caught during program execution and are usually referred to by a specific name:
+"""
+
+
+
+## Slide 7 ##
+"""
+The general syntax for catching exceptions
+"""
+# try:
+#     pass
+#     # Code that might raise an exception  (but runs if there is no exception)
+#
+# except Exception:
+#     pass
+#     # Code to handle the specific exception if it arises
+
+
+## Slide 8 ##
+"""
+We can keep the program from crashing if a file referenced cannot be found.
+"""
+# try:
+#     with open("nonexistent_file.txt", "r") as file:
+#         content = file.read()
+#         print(content)
+#
+# except Exception:
+#     print("Error: The file was not found. Please check the file name and try again.")
+
+## Slide 9 ##
+"""
+A very common exception is an IndexError
+"""
+
+
+
+## Slide 10 ##
+"""
+Multiple exceptions can occur during run time. We may want to handle each, based on WHAT the exception is.
+    The 'Exception' command often is too general to allow us to handle unique exceptions.
+    Here, we can get a type error from the user putting in a floar OR a division by zero error if zero is entered.
+        We would like to handle each differently.
+"""
+# try:
+#     num = int(input("Enter a number: "))
+#     print(10 / num)
+#
+# except Exception:
+#     print("An error occurred!")  # Not specific; hard to debug
+
+
+
+## Slide 11 ##
+""""
+Here, we handle each type of exception differently. 
+"""
+# try:
+#     num = int(input("Enter a number: "))
+#     print(10 / num)
+
+# pass
+
+
+## Slide 12 ##
+"""
+The 'raise <type>' command can be used to throw custom exceptions.
+This is useful when the program would otherwise interpret a command as valid but it must be handled as an error. 
+"""
+secret_code = 'secret'
+entered_code = input("Please enter the secret code: ")
+
+# pass
+
+
+## Slide 13 ##
+"""
+The 'raise' keyword looks for an 'except' keyword that tells it how the error should be handled.
+    Here, if an error occurs the 'except' is automatically triggered to handle the exception.
+        Since the only way to handle ValueError is to print 'InvalidInput' that is what's displayed regardless of the error.
+"""
+# age = input("Enter your age: ")
+#
+# try:
+#     if int(age) < 18:
+#         raise ValueError ("you are not old enough")
+#
+#     print("Age confirmed")
+#
+# except ValueError:
+#     print(f"Invalid input")
+
+
+## Slide 14 ##
+"""
+Here, after checking user input, if the user:
+    1) enters an invalid data type
+        - The conditional is skipped (since we don't have an int to check it against)
+        - The 'except' is triggered where the error raised by the invalid input is stored to the variable 'e'
+        - Invalid Input is printed with the text corresponding to the invalid data type
+    2) Enter a number less then 18
+        - The conditional triggers
+        - The 'except' triggers to handle the error and the text of the 'raise' is stored to the variable 'e'
+        - Invalid input is displayed with the text from the raise command ('you are not old enough')
+"""
+# age = input("Enter your age: ")
+#
+# try:
+#     if int(age) < 18:
+#         raise ValueError ("you are not old enough")
+#
+#     print("Age confirmed")
+#
+# except ValueError as e:
+#     print(f"Invalid input: {e}")
+#
+#
+#
+#
+
