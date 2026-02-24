aa7555418cc761ce0dca8835416ffabbc86487dd
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-24 13:15:56 -0600

    initial commit

---
+++ Unit_1/1_Defining Classes and Methods/car_lot.py
@@ -0,0 +1,48 @@
+pass # Import the Car class from the vehicle1 module
+
+pass # Create an instance of the car class as an object which we call 'my_car'
+pass # This returns the memory reference of the object created (we will look at making this more informative later)
+
+pass # Set the 'make' attribute of the car
+pass # Set the 'model' attribute of the car
+
+
+
+pass # Create another object from the Car() class
+
+
+
+
+###########################################################
+
+
+# Create an object by passing in attribute to the constructor (__init__)
+pass
+pass # We can see documentation for any object (or class) by using help
+
+# Objects have inherent methods that can be accessed and manipulated
+# Here, we have the ability to represent the attributes as dictionaries
+pass
+
+# Your turn - Add two more car objects by passing in the required parameters
+pass
+
+""" 
+    We can create objects using a loop as well.
+    Here we define the number of objects and create a list to hold them. 
+    
+    Then we add them using a loop
+"""
+pass
+
+
+#Print individual attributes for each car
+pass
+
+#We can also apply conditionals to our objects
+pass
+
+
+
+
+
---
+++ Unit_1/Exercises/1.1_create_your_own_class.py
@@ -0,0 +1,46 @@
+"""
+Exercise 1.1 - Create your Own Class
+
+Goal: Practice defining a class, writing an __init__ constructor, creating attributes, and declaring simple method definitions.
+
+Instructions:
+    You will design your own class based on an application you choose.
+    Examples of themes you may pick include:
+
+        A character in a video game
+        A robot in a competition
+        A Pokémon-style creature
+        A store product
+        A spaceship or vehicle
+        A pet or animal
+        A classroom tool or digital resource
+
+    Choose any idea you like, however, you will expand this idea in the next set of exercies,
+        so think about what idea might be interesting to develop as part of a full python program or game.
+
+STEP-BY-STEP:
+    STEP 1 — Name Your Class
+        Pick a name that represents your object.
+            Examples:
+                Player, Monster, PetDog, Robot, Spaceship, SoccerBall
+
+        Keep in mind that this name will be the 'template' for creating objects so keep more generic with the name.
+
+    STEP 2 — Define at least 6 attributes
+        Your attributes should describe your object.
+
+        They may include:
+            strings (name, type, category)
+            numbers (health, level, cost, weight, speed)
+            booleans (active, alive, unlocked)
+            lists (inventory, moves, upgrades)
+
+        Think - what things do you want ALL object for this class to have.
+        For example, maybe all spaceships have an attribute called 'fire rate'
+
+    STEP 3 — Define 3–5 method stubs
+        A method stub means you write the method name and parameters but leave the body as pass
+        Your methods should represent actions your object can perform.
+
+Use the vehicle1.py program as a template for how to approach this
+"""
