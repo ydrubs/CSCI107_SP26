de4a87b851b7fc72b975fd0649aac343ac9a006d
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-02 08:16:04 -0600

    initial commit

---
+++ Unit_1/2_Developing Classes and Methods/vehicle3.py
@@ -0,0 +1,10 @@
+"""
+In this program we start to implement the methods of our Car class.
+
+Methods are like functions within a class that allow an object to do an 'action' when called on the object
+
+We will practice passing in arguments into the methods in order to act on the data recieved
+"""
+
+class Car:
+    pass
