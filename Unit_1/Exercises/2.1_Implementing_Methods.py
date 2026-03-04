2b5e333b0633a5115a174ed10726e7ce5703dd41
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-04 08:16:52 -0600

    initial commit

---
+++ Unit_1/Exercises/2.1_Implementing_Methods.py
@@ -0,0 +1,29 @@
+"""
+Exercise 2.1 - Implementing Methods
+
+Goal: Implement at least one method in the class you previously designed in the previous exercises.
+
+STEP-BY-STEP:
+    STEP 1 — Choose AT LEAST TWO Methods You Wrote Earlier and Implement It (you can do more)
+        Your method MUST:
+            Receive at least one parameter
+            Modify at least one attribute
+            return a message or result
+
+            For example:
+                If your class was a GameCharacter a simple method could:
+                    def take_damage(self, amount):
+                        self.health -= amount
+                        return f"{self.name} now has {self.health} HP."
+
+    STEP 2 — Create an instance of the object and Call Your Methods separately
+
+    STEP 3 — Test Your Methods Thoroughly
+        Write at least three test cases - for example:
+
+            obj = MyClass("A", "B", "C", 10)
+            print(obj.take_damage(5))
+            print(obj.take_damage(10))
+            print(obj.take_damage(3))
+"""
+
