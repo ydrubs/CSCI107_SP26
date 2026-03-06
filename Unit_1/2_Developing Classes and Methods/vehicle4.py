ff0f67e31e339690febe9e3a7e17e1641c22cdc4
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-06 08:07:38 -0600

    initial commit

---
+++ Unit_1/2_Developing Classes and Methods/vehicle4.py
@@ -0,0 +1,16 @@
+"""
+In this program we explore how to control access to attributes and methods from the user.
+
+We explore using how to use attribute/method hiding and getter/setter methods.
+"""
+
+class Car:
+    def __init__(self, make, model, color, price, dealer_price, sold = False ):
+
+        # _ suggests that the attribute should not be changes outside of the class but still accessible outside the class in the normal way
+        pass
+
+        # Makes it so an attribute is harder to access (but not impossible) from outside the class
+        pass
+
+
