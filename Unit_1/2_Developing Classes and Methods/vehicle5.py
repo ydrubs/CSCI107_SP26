2b8edbf7fc9eb5285dd84f5c281e8fdb497ae4c4
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-09 08:25:40 -0500

    initial commit

---
+++ Unit_1/2_Developing Classes and Methods/vehicle5.py
@@ -0,0 +1,19 @@
+"""
+In this script we explore static methods and property decorators.
+
+A Python decorator is a special shortcut that lets you wrap extra features around an existing function—like adding a
+    new layer—without changing the function’s original code.
+
+They’re beneficial because decorators let you reuse common behavior across many functions without repeating code,
+    keeping your programs cleaner, easier to update, and more consistent.
+"""
+
+class Car():
+
+    pass# Define a static class method and make it private
+    pass #Increase the car count every time an instance is created
+
+
+    def __init__(self, make, model, color, price, dealer_price, sold = False):
+        # Increase count automatically whenever a new instance is created
+        pass
