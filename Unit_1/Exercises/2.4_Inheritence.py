b98528b5da5cc02870b4b22fd10e3999e61fac73
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-13 08:23:31 -0500

    initial commit

---
+++ Unit_1/Exercises/2.4_Inheritence.py
@@ -0,0 +1,23 @@
+"""
+2.4 Inheritence
+
+Goal: Extend your previous class by creating another class that is a child class of the current one
+
+STEP-BY_STEP:
+    STEP 1: Think of an IS-A relationship that could apply to your current class to form a child class.
+        For example: If you have a class called 'warrior' maybe a child class could be 'Samuari'
+
+    STEP 2: Write a new class that inherits from your old class
+        Add AT LEAST one attribute in your child class that your parent class does not have
+        Remember to properly initialize the child class and include super() to initialize the attributes in the parent class
+            See the vehicle6.py example of how we did this
+
+    STEP 3: Write a new method in your child class
+        The new method SHOULD be named the same as in your parent class but do something differnet
+
+            For example, if you have a method called attack in your parent class it should also be in your child class.
+                However, the two methods should NOT be identical
+
+    STEP 4: Demonstrate your implementation by creating an instance from the child class and calling the method
+"""
+
