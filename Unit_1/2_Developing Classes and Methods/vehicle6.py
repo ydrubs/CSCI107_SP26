49937a18854f0a9fc77291be8c8d1ad2283149e4
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-13 08:11:39 -0500

    initial commit

---
+++ Unit_1/2_Developing Classes and Methods/vehicle6.py
@@ -0,0 +1,11 @@
+"""
+This program demonstrates inheritance using a base class called Car.
+The Car class contains attributes and behaviors that all cars share.
+
+Two child classes inherit from Car:
+- GasolineCar
+- ElectricCar
+
+These child classes reuse the common functionality of Car and override
+the refuel() method to implement behavior specific to each type of car.
+"""
