180350aa86ae46c9c1c955a3f94ccc36ba896ae8
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-23 09:25:07 -0500

    3/23 update

--- Unit_1/2_Developing Classes and Methods/z_magic_methods_rational_numbers.py
+++ Unit_1/2_Developing Classes and Methods/z_magic_methods_rational_numbers.py
@@ -17,17 +17,22 @@ y = 3
     
 """
 
-pass # Result is 5
-pass # Result is 23
-pass # Result is ['2', '3'] since we added to lists together
+r1 = x + y # Result is 5
+r2 = str(x) + str(y) # Result is 23
+r3 = list(str(2)) + list(str(3)) # Result is ['2', '3'] since we added to lists together
+
+# print(r1)
+# print(r2)
+# print(r3)
 
 """
     The __.add__() operator knows how to hande '+' based on the class that the variable comes from (int, str, or list)
     Remember - everything in Python is treated as an object, x=3 is an instance of the integer class
-"""
-pass # Use the .add operator instead of the '+' symbol
-
-
+# """
+# print(x.__add__(y)) # Use the .add operator instead of the '+' symbol
+# print(x.__sub__(y))
+# # print(str(x).__sub__(str(y))) # ERROR
+# print(dir(str))
 
 ##################################################################################################################
 """"
@@ -41,8 +46,29 @@ operations work.
 """
 
 class RationalNumber:
-    pass
+    def __init__(self, numerator, denominator=1):
+        self.numerator = numerator
+        self.denominator = denominator
 
+    def __add__(self, other):
+        if not isinstance(other, RationalNumber):
+            other = RationalNumber(other)
+
+        numerator = self.numerator * other.denominator + self.denominator * other.numerator
+        denominator = self.denominator * other.denominator
+
+        # return f'{numerator}/{denominator}'
+        return RationalNumber(numerator, denominator)
+
+    def __sub__(self, other):
+
+        numerator1, denominator1 = self.numerator, self.denominator
+        numerator2, denominator2 = other.numerator, other.denominator
+
+        result_numerator = (numerator1 * denominator2 - numerator2*denominator1)
+        result_denomiantor = denominator1 * denominator2
+
+        return RationalNumber(result_numerator, result_denomiantor)
 
     """"
     Exercise - Write __mul__ and __trudiv__ methods below
@@ -53,8 +79,17 @@ class RationalNumber:
     """
     Output as a rational number rather then a memory reference 
     """
-
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
 
 
 if __name__ == "__main__":
-    pass
+    a = RationalNumber(1,2)
+    b = RationalNumber(7, 6)
+
+    print(a + b)
+    # print(3 + a)
+    print(isinstance(a, RationalNumber)) # True
+    print(isinstance(3, RationalNumber)) # False
+
+    # print(3 + a)
