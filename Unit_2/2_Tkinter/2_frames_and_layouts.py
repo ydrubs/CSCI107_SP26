f77d3e081f6feddd57e331d6159b32826f9726eb
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-17 09:27:02 -0500

    4/17 update

--- Unit_2/2_Tkinter/2_frames_and_layouts.py
+++ Unit_2/2_Tkinter/2_frames_and_layouts.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-pass
-pass  # Set the size of the root window
+root = tk.Tk()
+root.geometry('500x500')  # Set the size of the root window
 
 
 
@@ -10,7 +10,8 @@ color1 = '#F7F7F2'
 color2 = '#E4E6C3'
 color3 = '#899878'
 color4 = '#222725'
-color5 = '121113'
+color5 = '#121113'
+
 """
 Parameters:
 
@@ -18,8 +19,8 @@ Parameters:
 - fill='y' → allows the widget to expand vertically (top to bottom) to fill available space
 """
 # --- Sidebar
-pass
-pass
+sidebar = tk.Frame(root, width=80, bg=color3)
+sidebar.pack(side='left', fill='y')
 
 
 """
@@ -29,8 +30,8 @@ Parameters:
 -  expand=True → allows the widget to grow and take up extra space if the window is resized
 """
 # --- Main frame
-pass
-
+main_frame = tk.Frame(root, bg=color5)
+main_frame.pack(fill='both', expand=True)
 
 
 """
@@ -46,24 +47,24 @@ This happens because:
 
 Widgets packed later only get the remaining space.
 """
+frame1 = tk.Frame(main_frame, bg=color1, width=200, height=200)
+frame1.grid(row=0, column=0)
 
-pass
+frame2 = tk.Frame(main_frame, bg=color2, width=200, height=200)
+frame2.grid(row=0, column=1)
 
 
-# frame2 = tk.Frame(main_frame, bg='green', width=200, height=200)
-pass
 
-
-# frame3 = tk.Frame(main_frame, bg='yellow', width=200, height=200)
-pass
+frame3 = tk.Frame(main_frame, bg=color4, width=200, height=200)
+frame3.grid(row=1, column=1, sticky='ns')
 
 # Create a sidebar for frame 4
-# frame4 = tk.Frame(main_frame, bg='blue', height=100) # We don't need to specify width because of the sticky parameter (see below)
-pass
+frame4 = tk.Frame(main_frame, bg='blue') # We don't need to specify width because of the sticky parameter (see below)
+frame4.grid(row=1, column=0, sticky='nsew')
 
 #Create a footer that spans across both columns
-# frame5 = tk.Frame(main_frame, bg='tan', height=50)
-# frame5.grid(pass)
+frame5 = tk.Frame(main_frame, bg='tan', height=50)
+frame5.grid(row=2, column=0, columnspan=2, sticky='ew')
 
 """
 Grid Behavior: sticky and rowconfigure
@@ -88,7 +89,7 @@ Key Idea:
 
 Both are needed for a widget to fully expand.
 """
-pass
+main_frame.grid_rowconfigure(1, weight = 1)
 # main_frame.grid_rowconfigure(0, weight=2) # Allow for a 2:1 ratio for rows a and 2
 
 
@@ -128,21 +129,23 @@ Key Idea:
     Changing the anchor can cause parts of the widget to move or even
     extend outside the visible area of the parent.
 """
-pass
+child_frame = tk.Frame(frame1, bg='grey', width=50, height=50)
+child_frame.place(relx=0, rely=0)
 
-# child_frame2 = tk.Frame(frame2, bg='white', width=50, height=50)
-# child_frame2.place(pass)
+child_frame2 = tk.Frame(frame2, bg='white', width=50, height=50)
+child_frame2.place(relx=0.5, rely=0.5, anchor='center')
 
-# child_frame3 = tk.Frame(frame3, bg='white', width=50, height=50)
-# child_frame3.place(pass)
+child_frame3 = tk.Frame(frame3, bg='white', width=50, height=50)
+child_frame3.place(relx=0.5, rely=0, relwidth=0.1, relheight=0.5)
 
 # --- Create a sidebar for frame 4
-# child_frame4 = tk.Frame(frame4, bg='white', width=40, height=30)
-# child_frame4.pack(pass)
+child_frame4 = tk.Frame(frame4, bg='red', width=40, height=30)
+child_frame4.pack(side='right', fill='y')
 
 
 # --- Create text widgets inside frames
-pass
+text_frame1 = tk.Label(frame1, text='hi')
+text_frame1.place(relx=0.5, rely=0.5, anchor='center')
 
 
 """
@@ -163,17 +166,23 @@ Observe: If we comment out the pack_propagate command below we will see that the
 """
 
 # --- Example of creating a label nested inside a child frame
-# text_child_frame1 = tk.Label(child_frame1, text="Hi", fg='black', bg="pink")
-# text_child_frame1.pack(padx=5, pady=5)
-# pass # Try commenting this out to see the difference
+text_child_frame1 = tk.Label(child_frame, text="Hi", fg='black', bg="pink")
+text_child_frame1.pack(padx=5, pady=5)
+child_frame.pack_propagate(False) # Try commenting this out to see the difference
 
 
 """
 Student Challenge 
     Use grid to create 4 equal sized squares inside of child_frame2 (see image on whiteboard of what this should look like)
 """
-pass
+tk.Label(frame2, text ='some text').place(relx=0.5, rely=0.25, anchor='center')
+
+inner_child_frame2_1 = tk.Frame(child_frame2, bg='yellow', height=20, width=20)
+inner_child_frame2_1.grid(row=0, column=0)
+
+inner_child_frame2_2 = tk.Frame(child_frame2, bg='green', height=20, width=20)
+inner_child_frame2_2.grid(row=1, column=0)
 
 
-pass
+root.mainloop()
 
