7ded500e297e0c205059fe6e43c480d1848c0cfc
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-27 06:32:28 -0500

    initial commit

---
+++ Unit_2/2_Tkinter/5_Application_Example_2.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+pass ##This library is for image processing so we can resize an image
+
+pass # Makes the window
+pass #Makes the title that will appear in the top left
+pass # Define root color
+
+##Functions to draw shapes onto the canvas when called
+def red_circle():
+    pass # Clear the canvas
+    pass # Create Oval
+    pass # Place text in the box
+
+
+def yellow_rectangle():
+    pass
+
+def pikachu():
+    pass
+
+
+#Left Frame and its contents
+pass
+
+## Create some text
+pass
+# instructions = tk.Label(left_frame, text='1\n2\n3\n4\n5\n6\n7\n8\n9\n')
+pass
+
+
+##Try to open an image in current directory, print message of not found
+
+
+
+
+
+## Right Frame and its contents
+pass
+
+##Canvas for displaying shapes
+pass
+
+
+##A separate frame will hold the buttons to make placement easier
+pass
+
+# Buttons
+pass
+
+# yellow_button = tk.Button(btn_frame, text='Yellow', command=pass)
+# yellow_button.grid(row=0, column=1, padx=10, pady=2)
+
+# green_button = tk.Button(btn_frame, text='Green', command=pass)
+# green_button.grid(row=0, column=2, padx=10, pady=2)
+
+
+## Entry box for displaying the color of the shapes
+pass
+
+
+#start monitoring and updating the GUI
